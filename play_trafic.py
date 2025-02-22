7c0c86d6571f619451367228929b5142e318770f
CCChelios <palexanndr@gmail.com>
2025-02-22 12:34:52 +0300

    capture can trafic

--- play_trafic.py
+++ play_trafic.py
@@ -1,14 +1,15 @@
-# захват пакетов candump и вывод на экран
+# cannplayer candump log file
 import os
-import subprocess
-import time
 
 def setup_vcan_interface():
     os.system('sudo modprobe vcan')
     os.system('sudo ip link add dev vcan0 type vcan')
     os.system('sudo ip link set up vcan0')
-    
+
 def replay_can_traffic(log_file):
     os.system(f'canplayer -I {log_file} vcan0=can0 -l 50')
-    
-def capture_can_traffic(log_file):
+
+if __name__ == "__main__":
+    setup_vcan_interface()
+    log_file = 'example_candump.log'
+    replay_can_traffic(log_file)
